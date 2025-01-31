ff14867a1d42555d80c472b10aaadf8128808111
Clara Pastore <clarapastore@2a02-a44d-ceb6-1-9329-4112-4b24-e1a3.fixed6.kpn.net>
2025-01-31 15:46:46 +0100

    Third exercise introduction to js book

---
+++ introduction_javascript_book/basics/data_types.js
@@ -0,0 +1,31 @@
+// Identify the data type for each of the following values:
+// 'true'
+// false
+// 1.5
+// 2
+// undefined
+// { foo: "bar" }
+
+// To solve this, we can use the typeof operator
+
+typeof("true");
+// string
+
+typeof(false);
+// boolean
+
+typeof(1.5);
+// number
+
+typeof(2);
+// number
+
+typeof(undefined);
+// undefined
+
+typeof({ foo: 'bar' })
+// object
+
+// Note: Since typeof is an operator, you don't need to wrap the values in parentheses.
+
+// typeof "hello"
