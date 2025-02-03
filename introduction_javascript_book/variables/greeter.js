e1649a87d2423c2b07cef4e4de540169c37d6df1
Clara Pastore <clarapastore@2a02-a44d-ceb6-1-9329-4112-4b24-e1a3.fixed6.kpn.net>
2025-02-03 23:09:20 +0100

    First exercise Variables chapter

---
+++ introduction_javascript_book/variables/greeter.js
@@ -0,0 +1,11 @@
+// Write a program named greeter.js
+// The program greets "Victor" three times
+// Your program should use a variable and not hard code the string Victor in each greeting
+
+let firstName = "Victor";
+
+console.log(`Good Morning, ${firstName}.`);
+console.log(`Good Afternoon, ${firstName}.`);
+console.log(`Good Evening, ${firstName}.`);
+
+// Another solution is to use string concatenation instead of template literals. So, you would write console.log("Good Morning, " + firstName + ".");
